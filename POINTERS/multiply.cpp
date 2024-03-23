530e42f9796268c394baef07a0033b0d9add93b8
aashima2693 <157371227+aashima2693@users.noreply.github.com>
2024-03-23 19:36:14 +0530

    first commit

---
+++ POINTERS/multiply.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x , y;
+    cout<<"Enter the values:"<<endl;
+    cin>>x>>y;
+    int *ptrx =&x;
+      int *ptry =&y;
+
+      int result;
+      
+      int *ptr=&result;
+
+      *ptr = *ptrx * *ptry;
+
+      cout<<"RESULT="<<*ptr<<endl;
+
+    
+    return 0;
+}
