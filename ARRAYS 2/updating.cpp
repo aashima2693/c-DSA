250d4b920c451a7d027894b306e800ef5fbd44c1
aashima2693 <157371227+aashima2693@users.noreply.github.com>
2024-07-17 21:42:47 +0530

    First commit

---
+++ ARRAYS 2/updating.cpp
@@ -0,0 +1,24 @@
+// Change the second element of the array arr to 25 and print the updated array.
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5];
+    cout<<"Enter the elements of array:";
+    for(int i=0;i<5;i++){
+        cin>>arr[i];
+    }
+    cout<<endl;
+    cout<<"Array is:";
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<endl;
+    }
+    cout<<endl;
+    arr[1]=25;
+    cout<<"Updated array is:"<<endl;
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<endl;
+    }
+
+
+}
+
